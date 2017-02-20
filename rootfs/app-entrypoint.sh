b8d4ffe4dd7d63fe15d688f8fb8c37ed248501c5
Brian Flick <brian.flick21@gmail.com>
2017-02-20 11:06:37 -0800

    CHMOD files

--- rootfs/app-entrypoint.sh
+++ rootfs/app-entrypoint.sh
@@ -5,6 +5,8 @@
 print_welcome_page
 check_for_updates &
 
+chmod 644 /bitnami/mariadb/conf/my.conf
+
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
   nami_initialize mariadb
   info "Starting mariadb..."
